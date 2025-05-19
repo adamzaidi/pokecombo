a10b80e5ab942410dbbf9f691cba185f1e70bff2
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2025-05-18 22:20:14 -0400

    Update supabase.js

--- server/supabase.js
+++ server/supabase.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load variables from .env
 
 const supabase = createClient(
-  'https://tcewrqaqlbmfuzulvblo.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjZXdycWFxbGJtZnV6dWx2YmxvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MTA5ODIsImV4cCI6MjA2MzE4Njk4Mn0.CoI1385Tu9oPcHzIvu_pIAyZNdUuivffNPCRyjHS4rs'
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
 );
 
 export default supabase;
